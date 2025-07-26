c2923da2af8796d565967f058b32910664e8def1
navya <navyad30.03@gmail.com>
2025-07-26 18:51:03 +0530

    asdf

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,15 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> num;
-        int n= nums.size();
-        for(int i= 0; i< n; i++){
-            int rem= target- nums[i];
-            if(num.find(rem)!= num.end()){
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int rem = target - nums[i];
+            if (num.find(rem) != num.end()) {
                 return {num[rem], i};
             }
-            num[nums[i]]= i;
+            num[nums[i]] = i;
         }
         return {};
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No two sum solution found." << endl;
+    }
+
+    return 0;
+}
